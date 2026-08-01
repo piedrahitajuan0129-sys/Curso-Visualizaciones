94141adc4a5488dde59483d36ab0202de430969c
piedrahitajuan0129-sys <TU_CORREO_DE_GITHUB>
2026-08-01 10:19:02 -0500

    Agregar archivos del proyecto

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import openpyxl
+
+print("Todo funciona correctamente")
+pip list
+pip show pandas numpy matplotlib seaborn plotly scikit-learn openpyxl
+pip install pandas numpy matplotlib seaborn plotly scikit-learn scipy openpyxl jupyter notebook ipykernel
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import openpyxl
+
+print("Todo funciona correctamente")
